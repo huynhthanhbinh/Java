268c2eb7a76b2b32be7f0ef58585177d0135ac46
BHT <46950058+huynhthanhbinh@users.noreply.github.com>
2019-06-28 17:57:31 +0700

    Demo Stream (cont.)

--- src/app/DemoStream.java
+++ src/app/DemoStream.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class DemoStream {
@@ -40,14 +41,35 @@ public class DemoStream {
                 .add("b")
                 .add("c")
                 .build();
-
         streamBuilder.forEach(System.out::println);
-
         System.out.println();
+
+        Stream<String> streamGenerate = Stream
+                .generate(()->"element")
+                .limit(10);
+
+
+        streamGenerate.forEach(System.out::println);
+        System.out.println();
+
+        Stream<Integer> streamIterated = Stream
+                .iterate(10, n -> --n)
+                .limit(5);
+        streamIterated.forEach(System.out::println);
+        System.out.println();
+
 
         List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
         Integer reduced = integers.stream().reduce(23, (a, b) -> a - b);
 
         System.out.println(reduced);
+
+        String[] array = new String[]{"a", "b", "c"};
+        Stream<String> streamOfArray = Stream.of(array);
+        streamOfArray.forEach(System.out::println);
+
+        System.out.println();
+        IntStream s = "Richard Binh Huynh".chars();
+        s.mapToObj(i -> Character.toString((char) i)).forEach(System.out::print);
     }
 }
